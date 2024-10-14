3fff1cd9d5a4f0a495cc1d9c8ccd7be177673738
Enno841 <edwincali2002@gmail.com>
2024-10-14 15:05:42 +0800

    Changes to be committed: 	modified:   chirper/app/Http/Controllers/ChirpController.php 	modified:   chirper/app/Models/Chirp.php 	modified:   chirper/app/Models/User.php 	modified:   chirper/database/migrations/2024_10_14_064156_create_chirps_table.php 	modified:   chirper/resources/js/Layouts/AuthenticatedLayout.jsx 	new file:   chirper/resources/js/Pages/Chirps/Index.jsx 	deleted:    chirper/resources/js/Pages/Chirps/Index.vue

--- chirper/resources/js/Layouts/AuthenticatedLayout.jsx
+++ chirper/resources/js/Layouts/AuthenticatedLayout.jsx
@@ -26,6 +26,9 @@ export default function Authenticated({ header, children }) {
                                 <NavLink href={route('dashboard')} active={route().current('dashboard')}>
                                     Dashboard
                                 </NavLink>
+                                <NavLink href={route('chirps.index')} active={route().current('chirps.index')}>
+                                    Chirps
+                                </NavLink>
                             </div>
                         </div>
 
